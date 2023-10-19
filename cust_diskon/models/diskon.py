6b34da1e5694886f216437cd69ed92c08b440bac
riskafebrianti <riskaafebri@gmail.com>
2023-10-19 15:41:02 +0800

    saleorder(auto disc)

--- cust_diskon/models/diskon.py
+++ cust_diskon/models/diskon.py
@@ -1,37 +1,66 @@
 from odoo import models, fields, api
 
-
-class cust_diskon(models.Model):
+# ini buat di res_partner
+class cust_diskon2(models.Model):
     _inherit = 'res.partner'
-    # _description = 'cust_diskon.cust_diskon'
     diskon = fields.Integer('Diskon')
 
-#--
+# class cust_diskon1(models.Model):
+#     _inherit = 'res.partner'
+#     # _description = 'cust_diskon.cust_diskon'
+#     diskon = fields.Integer('Diskon')
+
+#--ini buat di order line nambahin dsc
 
 class cust_diskon(models.Model):
     _inherit = 'sale.order.line'
     # _description = 'cust_diskon.cust_diskon'
     # barcode = fields.Char('barcode',related='product_template_id.barcode')
-    barcode = fields.Char('barcode')
+    barcode = fields.Char ('barcode')
+
 
     @api.onchange("product_template_id")
     def _onchange_product_template_id(self):
         if self.product_template_id:
          self.barcode  = self.product_template_id.barcode
-
-    @api.model_create_multi
-    def create(self, vals_list):
-            res = super().create(vals_list)
-            if res.order_id.partner_id.diskon != 0:
-                res.discount = res.order_id.partner_id.diskon
-            return res
+         self.discount = self.order_id.partner_id.diskon
     
-class cust_diskonn(models.Model):
+    # @api.model_create_multi
+    # def create(self, vals_list):
+    #         res = super().create(vals_list)
+    #         if res.order_id.partner_id.diskon != 0:
+    #             res.discount = res.order_id.partner_id.diskon
+    #         return res
+
+class cust_diskon(models.Model):
     _inherit = 'sale.order'
     # _description = 'cust_diskon.cust_diskon'
-    diskonn = fields.Float('Diskon customer') 
+    diskon = fields.Integer('Diskon customer') 
+    
+    # buatautocomplite
+    @api.onchange("partner_id")
+    def _onchange_partner_id(self):
+            if self.partner_id:
+             self.diskon  = self.partner_id.diskon
+             
+            # self.discount  = self.partner_id.discount
+            # self.description = "Default description for %s" % (self.partner_id.diskon)
+    
+  
+    # ini buat nambahin field di saleorder
+# class cust_diskonn(models.Model):
+#     _inherit = 'sale.order'
+#     # _description = 'cust_diskon.cust_diskon'
+#     diskon = fields.Integer('Diskon customer') 
 
+class cust_diskon(models.Model):
+    _inherit = 'pos.order'
+    # _description = 'cust_diskon.cust_diskon'
+    # diskon = fields.Float('Diskon customer') 
+    
+    diskon = fields.Integer('diskon', related='partner_id.diskon')
 
+  
 # class cust_diskonsale(models.Model):
 #     _inherit = 'pos.order'
 #      _description = 'cust_diskon.cust_diskon'
@@ -45,14 +74,14 @@ class cust_diskonn(models.Model):
 #     # partner_id = fields.Many2one("res.partner", string="Partner")
 
 # buatautocomplite
-    @api.onchange("partner_id")
-    def _onchange_partner_id(self):
+@api.onchange("partner_id")
+def _onchange_partner_id(self):
         if self.partner_id:
          self.diskonn  = self.partner_id.diskon
         # self.discount  = self.partner_id.discount
         # self.description = "Default description for %s" % (self.partner_id.diskon)
         
-  
+
    
     # value = fields.Integer()
     # value2 = fields.Float(compute="_value_pc", store=True)
