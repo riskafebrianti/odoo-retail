877d4abb6c4d420948754cbad5e830c730587a4c
Bayuade <bayuadeirawan79@gmail.com>
2023-12-07 15:56:00 +0800

    new model

--- cust_diskon/models/__init__.py
+++ cust_diskon/models/__init__.py
@@ -3,8 +3,5 @@
 from . import models
 from . import diskon
 from . import barcode
-<<<<<<< HEAD
 from . import penagihan
-=======
 from . import lot
->>>>>>> ef9be2098942a1730edffba2fec3a15977020137
