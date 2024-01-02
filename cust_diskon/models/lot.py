10b40ed657cc048a1e0e616b74a03acc37b59015
Melly <riskaafebri@gmail.com>
2024-01-02 11:24:00 +0800

    tgl lot

--- cust_diskon/models/lot.py
+++ cust_diskon/models/lot.py
@@ -33,7 +33,7 @@ class StockMove(models.Model):
             month = current_date.month
 
             # Format the year and month as YYMM
-            formatted_date = "{:02d}{:02d}".format(month % 100, year)
+            formatted_date = "{:02d}".format(month) + str(year)[2:5]
             if self.has_tracking == 'lot':
                 if self.product_id.default_code:
                     if self.purchase_line_id.order_id.partner_id.kodevend:
